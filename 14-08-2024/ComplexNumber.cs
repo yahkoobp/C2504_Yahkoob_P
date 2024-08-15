e4584157b80fb10fe6982acf22d4aaf13858007b
YAHKOOB P <108567487+yahkoobp@users.noreply.github.com>
2024-08-15 23:07:41 +0530

    Create ComplexNumber.cs

---
+++ 14-08-2024/ComplexNumber.cs
@@ -0,0 +1,59 @@
+//class ComplexNumber
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexNumber
+{
+    internal class ComplexNumber
+    {
+        public int Real {  get; set; }
+        public int Imaginary { get; set; }
+
+        public ComplexNumber(int real , int imaginary) 
+        {
+            this.Real = real;
+            this.Imaginary = imaginary;
+        }
+
+        public ComplexNumber AddComplexNumber(ComplexNumber other)
+        {
+            ComplexNumber result = this;
+            this.Real += other.Real;
+            this.Imaginary += other.Imaginary;
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return $"{Real} + i{Imaginary}";
+        }
+    }
+}
+
+//Main()
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ComplexNumber cn1 = new ComplexNumber(3 , 4);
+            ComplexNumber cn2 = new ComplexNumber(5, 3);
+            ComplexNumber result = cn1.AddComplexNumber(cn2);
+            Console.WriteLine($"The resultant complex number after addition is {result}");
+
+        }
+    }
+}
+
