ed92a2d10e0ae2f76fc8a353a72a8ab1961f05f7
YAHKOOB P <108567487+yahkoobp@users.noreply.github.com>
2024-08-28 17:49:38 +0530

    Create ArraysAndStrings.cs

---
+++ 28-08-2024/ArraysAndStrings.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var str = "Hello world";
+            //Console.WriteLine(str.Trim());
+            //Console.WriteLine(str.TrimStart());
+            //Console.WriteLine(str.TrimEnd());
+            //Console.WriteLine(str.StartsWith(" "));
+            //Console.WriteLine(str.EndsWith("."));
+            //Console.WriteLine(str.Contains(" "));
+            //Console.WriteLine(str.Remove(0 , 3));
+            //Console.WriteLine(str.Replace(" " , "#"));
+            //Console.WriteLine(str.IndexOf("e"));
+            var nums = new int[] {1,2,3,4,5,6,7,8,9};
+            //Console.WriteLine(nums.Length);
+            //Console.WriteLine(nums.Last());
+            //Console.WriteLine(nums.First());
+            //Console.WriteLine(nums.Min());
+            //Console.WriteLine(nums.Max());
+            //Console.WriteLine(nums.ToString());
+            //Console.WriteLine(nums.Average());
+            //Console.WriteLine(nums.OrderBy(m => m));
+            //Console.WriteLine(nums.OrderByDescending(m => m));
+
+            //Array.Resize(ref nums, 20);
+            //Console.WriteLine(nums.Length);
+
+            //var list = new List<int>();
+            //foreach(int i in nums)
+            //{
+            //    list.Add(i);
+            //}
+            //var list = nums.ToList();
+            //Console.WriteLine(list);
+            //foreach(int i in list)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //var a = "abcdef";
+            //var b = "eghjfbcikl";
+            //var com = "";
+            //foreach (var ch in a)
+            //{
+            //    if (b.Contains(ch)) {
+            //        com += ch;
+            //    }
+            //}
+
+            //Console.WriteLine(com);
+        }
+    }
+}
